34c4302d27d166bd54465ac8f0814caa6ddf46ba
Matheus Fidelis <msfidelis@outlook.com>
2017-08-30 14:23:12 -0300

    remove build folder

--- app/build/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var http = require("http");
-var server = http.createServer();
-server.listen(3000, function () {
-    console.log("Server rodando na porta 3000");
-});
