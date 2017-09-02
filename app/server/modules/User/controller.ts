7c0aa7dde75b2128d5535cabf9c18c7a314234d3
Matheus Fidelis <msfidelis@outlook.com>
2017-09-02 01:22:41 -0300

    Testes

--- app/server/modules/User/controller.ts
+++ app/server/modules/User/controller.ts
@@ -37,7 +37,6 @@ class UserController {
         })
     }
 
-
 }
 
 export default UserController;
--- app/test/integration/integration.test.ts
+++ app/test/integration/integration.test.ts
@@ -3,14 +3,53 @@ import * as HTTPStatus from 'http-status';
 
 describe('Testes de integração', () => {
 
+	'use strict';
+
+	const config = require('../../server/config/config');
+	const UserModel = require('../../server/models');
+
+	let id;
+
+	const userTest = {
+		id: 100, 
+		name: "Matheus Fidelis", 
+		email: "msfidelis01@gmail.com",
+		password: "xupisco"
+	}
+
+	const userDefault = {
+		id: 1, 
+		name: "Default User", 
+		email: "default@default.com",
+		password: "default"
+	}
+
+	beforeEach((done) => {
+
+		UserModel.User.destroy({
+			where: {}
+		}).then(() => {
+			return UserModel.model.create(userDefault);
+		}).then(user => {
+			UserModel.User.create(userTest)
+			.then(() => {
+				done();
+			});
+		});
+
+	});
+
 	describe('GET /api/users/all', () => {
 
-		it("Deve retornar um JSON com todos os usuários", done => {
+		it("Deve retornar um Array com todos os usuários", done => {
 
 			request(app)
 				.get('/api/users/all')
 				.end((error, res) => {
 					expect(res.status).to.equal(HTTPStatus.OK);
+					expect(res.payload).to.be.an('array');
+					expect(res.payload[0].name).to.be.equal(userDefault.name)
+					expect(res.payload[0].email).to.be.equal(userDefault.email)
 					done(error);
 				});
 
@@ -23,9 +62,14 @@ describe('Testes de integração', () => {
 		it("Deve retornar as informações de um usuário específico", done => {
 
 			request(app)
-				.get(`/api/users/${1}`)
+				.get(`/api/users/${userDefault.id}`)
 				.end((error, res) => {
 					expect(res.status).to.equal(HTTPStatus.OK);
+					expect(res.body.payload.id).to.equal(userDefault.id)
+					expect(res.body.payload).to.have.all.keys([
+						'id', 'name', 'email', 'password'
+					]);
+					id = res.body.payload.id;
 					done(error);
 				});
 
@@ -39,7 +83,10 @@ describe('Testes de integração', () => {
 		it("Deve criar um usuário", done => {
 
 			const user = {
-				name: 'Teste'
+				id: 2,
+				name: 'Teste',
+				email: 'tchupis@loko.com',
+				password: '123'
 			};
 
 			request(app)
@@ -47,6 +94,9 @@ describe('Testes de integração', () => {
 				.send(user)
 				.end((error, res) => {
 					expect(res.status).to.equal(HTTPStatus.CREATED);
+					expect(res.body.payload.id).to.eql(user.id);
+					expect(res.body.payload.name).to.eql(user.name);
+					expect(res.body.payload.email).to.eql(user.email)
 					done(error);
 				});
 			
