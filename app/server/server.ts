21543fafdf0c665c0b1821f8b451836bb9dff34c
Matheus Fidelis <msfidelis@outlook.com>
2017-09-10 22:44:21 -0300

    cluster - melhorar

--- app/server/server.ts
+++ app/server/server.ts
@@ -1,31 +1,48 @@
+import * as cluster from 'cluster';
+import { cpus } from 'os';
 import * as http from 'http';
 import Api from './api/api';
 
 const models = require('./models');
 const config = require('./config/config')();
 
-const server = http.createServer(Api);
+let server;
 
 /**
- * Inicia o banco de dados e em seguida a aplicação
+ * Cluster Configs
  */
-models.sequelize.sync()
+const numCPUs = cpus().length;
 
-	.then(() => {
-
-		server.listen(config.serverPort);
-
-		server.on('listening', () => {
-			console.log(`Server rodando na porta ${config.serverPort}`);
-		});
-
-		server.on('error', (error: NodeJS.ErrnoException) => {
-			console.log(`Ocorreu um erro louco: ${error}`);
-		});
-
+if (cluster.isMaster) {
+	for (var i = 0; i < numCPUs; i++) {
+	  cluster.fork();
+	}
+	cluster.on('exit', (worker, code, signal) => {
+	  console.log(`worker ${worker.process.pid} died`);
 	});
+} else {
+	
+	const server = http.createServer(Api);
+	
+	/**
+	 * Inicia o banco de dados e em seguida a aplicação
+	 */
+	models.sequelize.sync()
+	
+		.then(() => {
+	
+			server.listen(config.serverPort);
+	
+			server.on('listening', () => {
+				console.log(`Server rodando na porta ${config.serverPort}`);
+			});
+	
+			server.on('error', (error: NodeJS.ErrnoException) => {
+				console.log(`Ocorreu um erro louco: ${error}`);
+			});
+	
+		});
+	
+}
 
 export default server;
-
-
-
